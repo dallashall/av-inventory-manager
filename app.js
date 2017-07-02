db504add0d74e355938519b8079dd1531a11e984
Dallas Hall <dallas.hall@gmail.com>
2017-07-01 21:46:35 -0700

    create basic running server

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-const options = {
-  root: __dirname,
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
-  }
-}
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.sendFile('./index.html', options);
+  res.status(200).send({message: 'Working'});
 });
 
-io.on('connection', function(socket) {
-  console.log('User connected..');
-});
-
-http.listen(3000, function() {
-  console.log('====================================');
-  console.log('Listening on port 3000');
-  console.log('====================================');
-});
+module.exports = app;
